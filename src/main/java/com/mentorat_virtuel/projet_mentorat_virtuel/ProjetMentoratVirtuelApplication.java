c3c7b1ed00e25d192848bdb738a6cc0dc2850dba
Allamine <allaminhafiz68@gmail.com>
2024-12-12 13:11:25 +0100

    projet mentorat virtuel

---
+++ src/main/java/com/mentorat_virtuel/projet_mentorat_virtuel/ProjetMentoratVirtuelApplication.java
@@ -0,0 +1,12 @@
+package com.mentorat_virtuel.projet_mentorat_virtuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMentoratVirtuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetMentoratVirtuelApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mentorat_virtuel/projet_mentorat_virtuel/ProjetMentoratVirtuelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentorat_virtuel.projet_mentorat_virtuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetMentoratVirtuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
