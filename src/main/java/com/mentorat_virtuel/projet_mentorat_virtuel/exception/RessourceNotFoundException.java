e6b9b279d6643a60e3481a32a87d353bacd0deb0
Allaminehafiz <allaminhafiz68@gmail.com>
2025-01-02 13:42:09 +0100

    ajout

--- src/main/java/com/mentorat_virtuel/projet_mentorat_virtuel/exception/RessourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mentorat_virtuel.projet_mentorat_virtuel.exception;
-
-public class RessourceNotFoundException extends RuntimeException{
-    public RessourceNotFoundException(String message) {
-        super(message);
-    }
-}
