e1d3347e18d2fab4293ebf30ee0e7ba3b4d88cb1
Allaminehafiz <allaminhafiz68@gmail.com>
2025-01-02 13:43:21 +0100

    ajout

--- src/main/java/com/mentorat_virtuel/projet_mentorat_virtuel/ErrorMessage.java
+++
@@ -1,6 +0,0 @@
-package com.mentorat_virtuel.projet_mentorat_virtuel;
-
-import java.util.Date;
-
-public record ErrorMessage(int status, String msg, String error, Date timestamp)  {
-}
--- src/main/java/com/mentorat_virtuel/projet_mentorat_virtuel/exception/RessourceExisteException.java
+++
@@ -1,7 +0,0 @@
-package com.mentorat_virtuel.projet_mentorat_virtuel.exception;
-
-public class RessourceExisteException extends RuntimeException {
-    public RessourceExisteException(String message){
-        super(message);
-    }
-}
