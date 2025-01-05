bdfee0d2984a178527fbc1906e5237ccef0ee2b5
Allaminehafiz <allaminhafiz68@gmail.com>
2025-01-05 12:34:10 +0100

    ajout

--- src/main/java/com/mentorat_virtuel/projet_mentorat_virtuel/exception/RessourceExistException.java
+++
@@ -1,7 +0,0 @@
-package com.mentorat_virtuel.projet_mentorat_virtuel.exception;
-
-public class RessourceExistException extends RuntimeException{
-    public RessourceExistException(String message) {
-        super(message);
-    }
-}
