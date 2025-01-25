84ab721aee187aaa11fa171185d31042ddecc691
herve fogang mpessa <hervempessa7@gmail.com>
2025-01-25 07:48:11 +0100

    documentation de API upload d'image

--- src/main/java/com/mentorat_virtuel/projet_mentorat_virtuel/controller/ImageController.java
+++ src/main/java/com/mentorat_virtuel/projet_mentorat_virtuel/controller/ImageController.java
@@ -2,6 +2,9 @@ package com.mentorat_virtuel.projet_mentorat_virtuel.controller;
 
 import com.mentorat_virtuel.projet_mentorat_virtuel.entity.Image;
 import com.mentorat_virtuel.projet_mentorat_virtuel.service.image.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +20,14 @@ public class ImageController {
     public ImageController(ImageService imageService) {
         this.imageService = imageService;
     }
-
+    @Operation(
+            summary = "permet de faire le telechargement d'ine image",
+            description = "Cette méthode permet d'ajouter un nouveau fichier  dans la base de données."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "fichier ajoutée avec succès"),
+            @ApiResponse(responseCode = "400", description = "Les données envoyées sont invalides")
+    })
     @PostMapping("image/upload")
     public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
         try {
@@ -27,6 +37,14 @@ public class ImageController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload image");
         }
     }
+    @Operation(
+            summary = "Récupérer un image par son identifiant",
+            description = "Cette méthode permet de récupérer un image spécifique en utilisant son identifiant unique dans la base de données."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "L'image a été récupéré avec succès."),
+            @ApiResponse(responseCode = "404", description = "image non trouvé pour l'ID donné.")
+    })
     @GetMapping("image/{id}")
     public ResponseEntity<byte[]> getImage(@PathVariable Integer id) {
         Image image = imageService.getImageById(id);
@@ -34,6 +52,14 @@ public class ImageController {
                 .contentType(MediaType.parseMediaType(image.getType()))
                 .body(new byte[]{image.getContent()});
     }
+    @Operation(
+            summary = "Récupérer une image par son nom",
+            description = "Cette méthode permet de récupérer une image spécifique en utilisant son nom dans la base de données."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "L'image a été récupéré avec succès."),
+            @ApiResponse(responseCode = "404", description = "l'image non trouvé pour le nom donné.")
+    })
     @GetMapping("image/{name}")
     public ResponseEntity<byte[]> getImageByName(@PathVariable String name) {
             Image image = imageService.getImageByName(name);
